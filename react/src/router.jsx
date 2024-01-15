a7c958f6f96378f15fe7cb250a769cc7306d2769
Hussein Alsheikh <hussein.jodd@gmail.com>
2024-01-15 14:26:28 +0300

    install react and laravel

---
+++ react/src/router.jsx
@@ -0,0 +1,51 @@
+import {Navigate, createBrowserRouter} from 'react-router-dom';
+import Login from './views/login';
+import Signup from './views/signup';
+import Users from './views/users';
+import NotFound from './views/NotFound';
+import DefaultLayout from './Components/DefaultLayout';
+import GuestLayout from './Components/GuestLayout';
+import Dashboard from './views/Dashboard';
+
+
+const router = createBrowserRouter([
+    {
+        path:'/' , 
+        element:<DefaultLayout/>,
+        children:[
+            {
+                path:'/',
+                element:<Navigate to="/users"/>
+            },
+            {
+                path:'/users',
+                element:<Users/>
+            },{
+                path:'/dashboard',
+                element:<Dashboard/>
+            }
+        ]
+    },
+    {
+        path:'/' , 
+        element:<GuestLayout/>,
+        children:[
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/signup',
+                element:<Signup/>
+            }
+        ]
+    },
+   
+    {
+        path:'*',
+        element:<NotFound/>
+    },
+]);
+
+
+export default router;
